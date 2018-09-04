77c6a997c035304af5395d365080ae6e04cfeefd
meyinM <15950565036@163.com>
2018-09-04 14:52:05 +0800

    first

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by 孙晓敏 on 2018/9/4.
+//  Copyright © 2018年 孙晓敏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
